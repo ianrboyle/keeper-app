152e3c84334239133fcd804016746b3ecf6d9b45
Ian Boyle <irboyle21@gmail.com>
2022-01-06 17:45:07 -0500

    adds ternary true or false option for if isLoggedIn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,15 +5,18 @@ import Footer from "./Footer"
 import notes from "../notes"
 import Login from "./Login"
 
+var isLoggedIn = false;
 
 function App() {
-  return <div><Heading />
-       <h1>Hello</h1>
-      <Login />
-   {notes.map((note) =>  <Note key={note.id} title={note.title} content={note.content.substring(0,75) + "..."} />
+  return <div>
+    
+    
+    {isLoggedIn ? <Heading /> : <Login />}
+    {notes.map((note) =>  <Note key={note.id} title={note.title} content={note.content.substring(0,75) + "..."}  />
 )}
  
-<Footer /></div>
+{/* <Footer /> */}
+</div>
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,12 +2,17 @@ import React from "react";
 import Input from "./Input";
 function Login() {
   return (
-    <div className="container">
-      <form className="form">
-        <Input type="text" placeholder="Username" />
-        <Input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+    <div>
+      <header>
+        <h1>Welcome</h1>
+      </header>
+      <div className="container">
+        <form className="form">
+          <Input type="text" placeholder="Username" />
+          <Input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 }
