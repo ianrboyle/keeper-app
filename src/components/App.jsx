1da49c58f2022f91c6d6f1ad655eac100f2d89ea
Ian Boyle <irboyle21@gmail.com>
2022-01-06 17:27:38 -0500

    adds Login component and css for login/inputs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,13 @@ import Heading from "./Heading"
 import Note from "./Note"
 import Footer from "./Footer"
 import notes from "../notes"
-
+import Login from "./Login"
 
 
 function App() {
   return <div><Heading />
+       <h1>Hello</h1>
+      <Login />
    {notes.map((note) =>  <Note key={note.id} title={note.title} content={note.content.substring(0,75) + "..."} />
 )}
  
