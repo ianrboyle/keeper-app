c1cb20e45aa27bee4addb6966177f291197423b2
Ian Boyle <irboyle21@gmail.com>
2022-01-06 17:10:20 -0500

    cleans up code. cleans up map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,30 +3,15 @@ import Heading from "./Heading"
 import Note from "./Note"
 import Footer from "./Footer"
 import notes from "../notes"
-// import Card from "./Card"
-// import contacts from "../contacts.js"
 
-// function createCard(contact) {
-//   return <Card key={contact.id} name={contact.name} imgURL={contact.imgURL} phone={contact.phone} email={contact.email}/>
-// }
 
-function createNote(note) {
-  return <Note ket={note.id} title={note.title} content={note.content} />
-}
+
 function App() {
   return <div><Heading />
-   {notes.map(createNote)}
-   {/* {contacts.map(createCard)} */}
-   {/* <Card
-  name="Me"
-  tel="123-456-7890"
-  img="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ftravelsandliving.com%2Fwp-content%2Fuploads%2F2016%2F01%2Fadorable-smiling-dogs-21.jpg&f=1&nofb=1"
-/>
-<Card
-  name="Ian"
-  tel="123-123-1234"
-  img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2014%2F10%2Fanimal%2FCute_Golden_Dog.jpg&f=1&nofb=1"
-/> */}
+   {notes.map((note) => {
+  return <Note key={note.id} title={note.title} content={note.content.substring(0,75) + "..."} />
+})}
+ 
 <Footer /></div>
 }
 
