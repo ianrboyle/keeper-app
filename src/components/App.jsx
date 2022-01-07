aee8bf0ec9c7f3cb0d77d68984b9db4a7014235f
Ian Boyle <irboyle21@gmail.com>
2022-01-07 08:16:42 -0500

    adds register component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,21 @@ import Note from "./Note"
 import Footer from "./Footer"
 import notes from "../notes"
 import Login from "./Login"
+// import Register from "./Register"
 
 var isLoggedIn = false;
+var isRegistered = false;
 
 function App() {
   return <div>
+    <Heading />
     
-    
-    {isLoggedIn ? <Heading /> : <Login />}
+    {isLoggedIn ? null : <Login isRegistered={isRegistered}/>}
+    {/* {isRegistered ? <Register /> : <Login />} */}
     {notes.map((note) =>  <Note key={note.id} title={note.title} content={note.content.substring(0,75) + "..."}  />
 )}
  
-{/* <Footer /> */}
+<Footer />
 </div>
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,19 +1,21 @@
 import React from "react";
 import Input from "./Input";
-function Login(props) {
+function Register() {
   return (
     <div>
+      <header>
+        <h1>Welcome</h1>
+      </header>
       <div className="container">
         <form className="form">
           <Input type="text" placeholder="Username" />
           <Input type="password" placeholder="Password" />
-          {props.isRegistered === false && <Input type="password" placeholder="Confirm Password" />}
-
-          <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
+          <Input type="password" placeholder="Confirm Password" />
+          <button type="submit">Register</button>
         </form>
       </div>
     </div>
   );
 }
 
-export default Login;
+export default Register;
