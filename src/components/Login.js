bf631b16536584b483dd5e1bfe155f1b876d46a1
Ian Boyle <irboyle21@gmail.com>
2022-01-08 09:07:21 -0500

    adds mouse over/ mouse out event to login // register button in login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "./Input";
 function Login(props) {
+  const [isMousedOver, setMouseOver] = useState(false);
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
   return (
     <div>
       <div className="container">
@@ -8,7 +17,14 @@ function Login(props) {
           <Input type="text" placeholder="Username" />
           <Input type="password" placeholder="Password" />
           {props.isRegistered === false && <Input type="password" placeholder="Confirm Password" />}
-          <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
+          <button
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+            style={{ backgroundColor: isMousedOver ? "rgb(155, 223, 151)" : "rgb(237, 227, 149)" }}
+            type="submit"
+          >
+            {props.isRegistered ? "Login" : "Register"}
+          </button>
         </form>
       </div>
     </div>
