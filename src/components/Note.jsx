0d7bef682b3de4f0dfd63c4280c7698ffa950bfd
Ian Boyle <irboyle21@gmail.com>
2022-01-08 09:00:22 -0500

    adds on mouse out event to note component

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -8,10 +8,14 @@ import React, {useState} from "react";
 function Note(props) {
   const [isMousedOver, setMouseOver] = useState(false)
 
-  function handleMouseOver(note){
+  function handleMouseOver(){
     setMouseOver(true)
   }
-  return <div onMouseOver={handleMouseOver} style={{backgroundColor: isMousedOver ? "lightcyan" : "white"}} className="note"><h1>{props.title}</h1><p>{props.content}</p>
+
+  function handleMouseOut() {
+    setMouseOver(false)
+  }
+  return <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} style={{backgroundColor: isMousedOver ? "lightcyan" : "white"}} className="note"><h1>{props.title}</h1><p>{props.content}</p>
 
   </div>
 }
