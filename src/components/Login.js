47a7da591ce199e5d3cfae8a5722f2dc8310625c
Ian Boyle <irboyle21@gmail.com>
2022-01-08 09:44:54 -0500

    adds event.preventDefault() to on click method in Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,26 +14,25 @@ function Login(props) {
   const [name, setName] = useState("");
   // const handleNameChange = (event) => setName(event.target.value);
   function handleChange(event) {
-    console.log(event.target.value);
     setName(event.target.value);
   }
 
   const [headingName, setHeadingName] = useState("");
 
-  function handleClick() {
+  function handleClick(event) {
     setHeadingName(name);
+    event.preventDefault();
   }
   return (
     <div>
       <div className="container">
-        <form className="form">
+        <form className="form" onSubmit={handleClick}>
           <h1>Hello {headingName}</h1>
           <input type="text" onChange={handleChange} value={name} placeholder="Your Name" />
           <Input type="text" placeholder="Username" />
           <Input type="password" placeholder="Password" />
           {props.isRegistered === false && <Input type="password" placeholder="Confirm Password" />}
           <button
-            onClick={handleClick}
             onMouseOver={handleMouseOver}
             onMouseOut={handleMouseOut}
             style={{ backgroundColor: isMousedOver ? "rgb(155, 223, 151)" : "rgb(237, 227, 149)" }}
